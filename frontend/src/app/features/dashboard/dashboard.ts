b037c3de6c61f79dc7e8a2eb07eb80f1c59d56a9
portlexs <portnyagin924@gmail.com>
2025-07-28 12:49:58 +0600

    feat: connect dashboard to widgetService and widgeetRenderer

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,25 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {
-  CompactType,
-  GridsterConfig,
-  GridsterItem,
-  GridsterModule,
-  GridType,
-} from 'angular-gridster2';
+import { GridsterConfig, GridsterModule, GridType } from 'angular-gridster2';
+import { DashboardWidget } from '../widgets/widgets.interface';
+import { WidgetsService } from '../widgets/widgets.service';
+import { WidgetRenderer } from '../widgets/widget-renderer/widget-renderer';
 
 @Component({
   selector: 'dashboard',
   standalone: true,
-  imports: [CommonModule, GridsterModule],
+  imports: [CommonModule, GridsterModule, WidgetRenderer],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard implements OnInit {
-  options!: GridsterConfig;
-  dashboardWidgets?: Array<GridsterItem>;
+  options: GridsterConfig = {};
+  dashboardWidgets: DashboardWidget[] = [];
+
+  constructor(private widgetsService: WidgetsService) {}
 
   ngOnInit(): void {
+    this.initGridsterOptions();
+
+    this.widgetsService.widgets$.subscribe((widgets) => {
+      this.dashboardWidgets = widgets;
+    });
+  }
+
+  initGridsterOptions(): void {
     this.options = {
       gridType: GridType.Fixed,
 
@@ -43,11 +50,5 @@ export class Dashboard implements OnInit {
         },
       },
     };
-
-    this.dashboardWidgets = [
-      { cols: 1, rows: 1, y: 0, x: 0 },
-      { cols: 1, rows: 1, y: 1, x: 1 },
-      { cols: 1, rows: 1, y: 2, x: 2 },
-    ];
   }
 }
