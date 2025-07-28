ab20e78bab4eaf2feda4a5fd154b3d744dee31b1
portlexs <portnyagin924@gmail.com>
2025-07-28 12:32:06 +0600

    feat: add text widget component

---
+++ frontend/src/app/features/widgets/text-widget/text-widget.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-widget',
+  imports: [],
+  templateUrl: './text-widget.html',
+  styleUrl: './text-widget.css'
+})
+export class TextWidget {
+
+}
