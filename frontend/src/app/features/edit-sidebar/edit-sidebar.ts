193a218f258c2e0af6156ac4b7cb4b804a220e79
KiriLLchik07 <kirill3456777@mail.ru>
2025-07-31 15:41:29 +0600

    feat: dynamic component creating in edit-sidebar is complete

--- frontend/src/app/features/edit-sidebar/edit-sidebar.ts
+++ frontend/src/app/features/edit-sidebar/edit-sidebar.ts
@@ -7,11 +7,11 @@ import {
   ComponentRef,
 } from '@angular/core';
 import { EditSidebarService } from '../../services/edit-sidebar.service';
-import { Subject } from 'rxjs';
 import { LucideAngularModule, LucideIconData, X } from 'lucide-angular';
 import { Subscription } from 'rxjs';
 import { WidgetService } from '../../services/widget.service';
 import { widgetEditorsMap } from '../../configs/widget-editors-map';
+import { Widget } from '../../interfaces/widget.interface';
 
 @Component({
   selector: 'edit-sidebar',
@@ -29,6 +29,7 @@ export class EditSidebar implements OnInit, OnDestroy {
   editorContainer!: ViewContainerRef;
   private componentRef?: ComponentRef<any>;
   private subscriptions = new Subscription();
+  private widgetsList: Widget[] = [];
 
   constructor(
     private editsidebarServise: EditSidebarService,
@@ -42,12 +43,17 @@ export class EditSidebar implements OnInit, OnDestroy {
       })
     );
 
-    // Подписываемся на id редактируемого виджета
     this.subscriptions.add(
       this.editsidebarServise.widgetEditableId$.subscribe((widgetId) => {
         this.widgetId = widgetId;
 
         this.loadEditorComponent(widgetId);
+      })
+    );
+
+    this.subscriptions.add(
+      this.widgetService.widgets$.subscribe((widgets) => {
+        this.widgetsList = widgets;
       })
     );
   }
@@ -62,33 +68,25 @@ export class EditSidebar implements OnInit, OnDestroy {
       return;
     }
 
-    this.subscriptions.add(
-      this.widgetService.widgets$.subscribe((widgets) => {
-        const widget = widgets.find((w) => w.id === widgetId);
+    const widget = this.widgetsList.find((w) => w.id === widgetId);
 
-        if (!widget) {
-          console.warn('Widget not found with id:', widgetId);
-          return;
-        }
+    if (!widget) {
+      console.warn('Widget not found with id:', widgetId);
+      return;
+    }
 
-        const editorComponent = widgetEditorsMap[widget.type];
+    const editorComponent = widgetEditorsMap[widget.type];
 
-        if (!editorComponent) {
-          console.warn(
-            'No editor component found for widget type:',
-            widget.type
-          );
-          return;
-        }
+    if (!editorComponent) {
+      console.warn('No editor component found for widget type:', widget.type);
+      return;
+    }
 
-        this.componentRef =
-          this.editorContainer.createComponent(editorComponent);
+    this.componentRef = this.editorContainer.createComponent(editorComponent);
 
-        if (this.componentRef.instance) {
-          this.componentRef.instance.widget = widget;
-        }
-      })
-    );
+    if (this.componentRef.instance) {
+      this.componentRef.instance.widget = widget;
+    }
   }
 
   ngOnDestroy() {
