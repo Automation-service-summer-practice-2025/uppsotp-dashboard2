bb548cac03890c8a99025bb55fb117b8fae2a809
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-07-31 11:08:05 +0600

    feat: add text editor component

---
+++ frontend/src/app/editors/text-widget-editor/text-widget-editor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-widget-editor',
+  imports: [],
+  templateUrl: './text-widget-editor.html',
+  styleUrl: './text-widget-editor.css'
+})
+export class TextWidgetEditor {
+
+}
