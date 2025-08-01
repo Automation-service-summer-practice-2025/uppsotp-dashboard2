60ec70b294bd4501b3cbdfb1ec7ad90435174be2
portlexs <portnyagin924@gmail.com>
2025-08-01 21:49:25 +0600

    feat: add text-widget-editor and image-widget-editor

---
+++ frontend/src/app/features/editors/image-widget-editor/image-widget-editor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'image-widget-editor',
+  imports: [],
+  templateUrl: './image-widget-editor.html',
+  styleUrl: './image-widget-editor.css',
+})
+export class ImageWidgetEditor {}
---
+++ frontend/src/app/features/editors/text-widget-editor/text-widget-editor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'text-widget-editor',
+  imports: [],
+  templateUrl: './text-widget-editor.html',
+  styleUrl: './text-widget-editor.css',
+})
+export class TextWidgetEditor {}
