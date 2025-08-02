0d8e661d5ca9c214c958a071b628980d112f7246
portlexs <portnyagin924@gmail.com>
2025-08-02 18:29:38 +0600

    feat: add editor-render and widget-render

---
+++ frontend/src/app/features/editors/editor-render/editor-render.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'editor-render',
+  imports: [],
+  templateUrl: './editor-render.html',
+  styleUrl: './editor-render.css',
+})
+export class EditorRender {}
---
+++ frontend/src/app/features/widgets/widget-render/widget-render.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'widget-render',
+  imports: [],
+  templateUrl: './widget-render.html',
+  styleUrl: './widget-render.css',
+})
+export class WidgetRender {}
