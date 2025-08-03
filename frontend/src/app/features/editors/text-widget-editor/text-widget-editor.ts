b20cd5ba1d5b89cee1b986edc51f55b41a254849
portlexs <portnyagin924@gmail.com>
2025-08-03 19:46:22 +0600

    feat: add text-widget-editor example

--- frontend/src/app/features/editors/text-widget-editor/text-widget-editor.ts
+++ frontend/src/app/features/editors/text-widget-editor/text-widget-editor.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TextWidget } from '../../../interfaces/widget-classes';
 
 @Component({
   selector: 'text-widget-editor',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './text-widget-editor.html',
   styleUrl: './text-widget-editor.css',
 })
