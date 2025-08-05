9ddb079028601b97480798ed0a69641e51fbfacc
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-08-05 10:36:26 +0600

    feat: add creat and set editor in text-widget

--- frontend/src/app/features/widgets/text-widget/text-widget.ts
+++ frontend/src/app/features/widgets/text-widget/text-widget.ts
@@ -1,12 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { TextWidget } from '../../../interfaces/widget-classes';
+import { Editor, NgxEditorComponent } from 'ngx-editor';
+import { FormsModule } from '@angular/forms';
+import { EditSidebarService } from '../../../services/edit-sidebar.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'text-widget',
-  imports: [],
+  imports: [NgxEditorComponent, FormsModule],
   templateUrl: './text-widget.html',
   styleUrl: './text-widget.css',
 })
 export class TextWidgetComponent {
   @Input() widget!: TextWidget;
+
+  editor!: Editor;
+
+  constructor(private editSidebarService: EditSidebarService) {}
+
+  ngOnInit(): void {
+    this.editor = new Editor();
+    this.editSidebarService.setEditor(this.editor);
+    console.log('Editor создан');
+  }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+    // this.editSidebarService.setEditor(); // Очищаем при уничтожении
+  }
 }
