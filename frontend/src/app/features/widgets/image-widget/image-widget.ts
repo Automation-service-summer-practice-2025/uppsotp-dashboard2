48369b15a0575e319eea8b5f62a9505ea07acd44
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-08-06 13:04:13 +0600

    feat: add icon image on image-widget

--- frontend/src/app/features/widgets/image-widget/image-widget.ts
+++ frontend/src/app/features/widgets/image-widget/image-widget.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ImageWidget } from '../../../interfaces/widget-classes';
+import { Image, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'image-widget',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './image-widget.html',
   styleUrl: './image-widget.css',
 })
 export class ImageWidgetComponent {
   @Input() widget!: ImageWidget;
+
+  readonly iconImage = Image;
 }
