2a6cd9f5ef0e002483263f10527fefa3ae5f5aaa
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-08-06 10:47:36 +0600

    feat: new realization

--- frontend/src/app/interfaces/widget-classes.ts
+++ frontend/src/app/interfaces/widget-classes.ts
@@ -1,8 +1,10 @@
+import { Editor } from 'ngx-editor';
 import { Widget } from './widget.interface';
 
 export class TextWidget extends Widget {
   override type: string = 'text';
   htmlContent: string = '';
+  editor: Editor = new Editor();
 }
 
 export class ImageWidget extends Widget {
