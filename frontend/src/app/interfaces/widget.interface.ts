2154537dd21c364028d7d22c04c53d57b8587ce7
portlexs <portnyagin924@gmail.com>
2025-08-10 18:45:09 +0600

    fix: delete IWidget interface

--- frontend/src/app/interfaces/widget.interface.ts
+++ frontend/src/app/interfaces/widget.interface.ts
@@ -2,24 +2,20 @@ import { Type } from '@angular/core';
 import { GridsterItem } from 'angular-gridster2';
 import { v4 as uuidv4 } from 'uuid';
 
-export interface IWidget extends GridsterItem {
-  id: string;
-  type: string;
-}
-
 export interface WidgetConfig {
   Component: Type<any>;
   Editor: Type<any>;
   Widget: Type<Widget>;
 }
 
-export class Widget implements IWidget {
+export class Widget implements GridsterItem {
   id: string;
   type!: string;
   x: number = 0;
   y: number = 0;
   cols: number = 3;
   rows: number = 3;
+  dragEnabled: boolean = true;
 
   constructor() {
     this.id = uuidv4();
