291d01ccd20de2810533f6399e732f7703fe45b1
dreameat3r <wildlynx14425@gmail.com>
2025-08-11 14:15:46 +0600

    .gitignore added removed unnecessary files

--- backend/Models/TextWidget.cs
+++
@@ -1,9 +0,0 @@
-namespace Models;
-
-public class TextWidget
-{
-    public Guid WidgetId { get; set; }
-    public string HtmlContent { get; set; } = "";
-
-    public required Widget Widget { get; set; }
-}
