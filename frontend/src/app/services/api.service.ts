e0741950fa18a0b8ad7393c6213cc53962ddeacf
portlexs <portnyagin924@gmail.com>
2025-08-14 09:28:32 +0600

    feat: update apiUrl and add CRUD methods in api.service.ts

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Widget } from '../interfaces/widget.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private readonly baseUrl = 'http://localhost:5062/api';
+  private readonly apiUrl = 'http://localhost:5062';
 
   constructor(private http: HttpClient) {}
 
-  // TODO: Реализовать CRUD операции для виджетов
-  // TODO: Реализовать загрузку файлов для ImageWidget
+  getWidgets(): Observable<Widget[]> {
+    throw new Error('Method not implemented');
+  }
+
+  createWidget(widget: Widget): Observable<Widget> {
+    throw new Error('Method not implemented');
+  }
+
+  updateWidget(widget: Widget): Observable<Widget> {
+    throw new Error('Method not implemented');
+  }
+
+  deleteWidget(widget: Widget): Observable<Widget> {
+    throw new Error('Method not implemented');
+  }
 }
