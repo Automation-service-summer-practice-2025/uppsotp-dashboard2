386c7ce09b537e8b11d182a1b77f89c4f0064f07
portlexs <portnyagin924@gmail.com>
2025-08-15 14:35:27 +0600

    feat: add saving when focuse changed

--- frontend/src/app/services/edit-sidebar.service.ts
+++ frontend/src/app/services/edit-sidebar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Widget } from '../interfaces/widget.interface';
+import { WidgetService } from './widget.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,15 +13,27 @@ export class EditSidebarService {
   isOpen$ = this.isOpenEditSidebar.asObservable();
   currentWidget$ = this.currentWidget.asObservable();
 
+  constructor(private widgetService: WidgetService) {}
+
   openEditSidebar(widget: Widget): void {
     const currentWidget = this.currentWidget.getValue();
-    if (!currentWidget || widget.id != currentWidget.id) {
-      this.isOpenEditSidebar.next(true);
-      this.currentWidget.next(widget);
+
+    if (currentWidget && currentWidget.id === widget.id) {
+      return;
+    } else if (currentWidget) {
+      this.widgetService.saveWidget(currentWidget);
     }
+    this.isOpenEditSidebar.next(true);
+    this.currentWidget.next(widget);
   }
 
   closeEditSidebar(): void {
+    const currentWidget = this.currentWidget.getValue();
+
+    if (currentWidget) {
+      this.widgetService.saveWidget(currentWidget);
+    }
+
     this.isOpenEditSidebar.next(false);
     this.currentWidget.next(undefined);
   }
