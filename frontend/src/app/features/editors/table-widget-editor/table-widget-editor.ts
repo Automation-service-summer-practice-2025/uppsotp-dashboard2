73887a6b4660486903cdcc46abff9ca41f8016d6
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-08-18 12:21:45 +0600

    fix: move availableThemes in config

--- frontend/src/app/features/editors/table-widget-editor/table-widget-editor.ts
+++ frontend/src/app/features/editors/table-widget-editor/table-widget-editor.ts
@@ -1,23 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { TableWidget } from '../../../interfaces/widget-classes';
-import {
-  ColDef,
-  colorSchemeDarkBlue,
-  colorSchemeDarkWarm,
-  colorSchemeLightCold,
-  colorSchemeLightWarm,
-  themeBalham,
-  themeMaterial,
-  themeQuartz,
-} from 'ag-grid-community';
-import {
-  LucideAngularModule,
-  Minus,
-  Plus,
-  StretchHorizontal,
-  StretchVertical,
-} from 'lucide-angular';
+import { ColDef } from 'ag-grid-community';
+import { LucideAngularModule } from 'lucide-angular';
 import { getTableEditorButtons } from '../../../configs/table-editor-btn.config';
+import { availableThemes } from '../../../configs/table-availablethemes.config';
 
 @Component({
   selector: 'table-widget-editor',
@@ -32,40 +18,7 @@ export class TableWidgetEditor {
   private nextColId = 1;
   private nextRowId = 1;
 
-  readonly availableThemes = [
-    {
-      name: 'Quartz Dark Blue',
-      theme: themeQuartz.withPart(colorSchemeDarkBlue),
-    },
-    {
-      name: 'Quartz Dark Warm',
-      theme: themeQuartz.withPart(colorSchemeDarkWarm),
-    },
-    {
-      name: 'Quartz Light Warm',
-      theme: themeQuartz.withPart(colorSchemeLightWarm),
-    },
-    {
-      name: 'Quartz Light Cold',
-      theme: themeQuartz.withPart(colorSchemeLightCold),
-    },
-    {
-      name: 'Balham Light',
-      theme: themeBalham.withPart(colorSchemeLightWarm),
-    },
-    {
-      name: 'Balham Dark',
-      theme: themeBalham.withPart(colorSchemeDarkWarm),
-    },
-    {
-      name: 'Material Light',
-      theme: themeMaterial.withPart(colorSchemeLightCold),
-    },
-    {
-      name: 'Material Dark',
-      theme: themeMaterial.withPart(colorSchemeDarkBlue),
-    },
-  ];
+  readonly availableThemes = availableThemes;
 
   get buttons() {
     return getTableEditorButtons({
