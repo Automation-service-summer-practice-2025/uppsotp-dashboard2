5e7bb19b3eeda75a1193a476e546444e11431c04
KiriLLchik07 <kirill3456777@mail.ru>
2025-08-18 15:11:46 +0600

    feat: add hint for upload file name

--- frontend/src/app/features/editors/chart-widget-editor/chart-widget-editor.ts
+++ frontend/src/app/features/editors/chart-widget-editor/chart-widget-editor.ts
@@ -18,6 +18,7 @@ export class ChartWidgetEditor implements OnInit {
 
   csvData: any[] = [];
   csvHeaders: string[] = [];
+  uploadedFileName: string = '';
 
   chartFeatureSelectors = chartFeatureSelectors;
 
@@ -54,6 +55,8 @@ export class ChartWidgetEditor implements OnInit {
   handleFileInput(event: any) {
     const file = event.target.files[0];
     if (!file) return;
+
+    this.uploadedFileName = file.name;
 
     const reader = new FileReader();
     reader.onload = () => {
